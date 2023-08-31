a905c672b2f3b166f6ab288b59e7189084a7c424
raquelgarciat <raquelgarciat@gmail.com>
2023-08-31 17:42:53 +0200

    add hamburger nav bar

--- src/components/App.js
+++ src/components/App.js
@@ -8,28 +8,32 @@ function App() {
       <header className='header'>
         <img src={headerpic} alt='logo' className='header_logo' />
       </header>
-      
-        <nav className='nav'>
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Projects</li>
-            <li>Contact</li>
+      <nav role='navigation' className='navbar'>
+        <div id='menuToggle'>
+          <input type='checkbox' />
+          <span></span>
+          <span></span>
+          <span></span>
+          <ul id='menu'>
+            <a href='/'>
+              <li>Home</li>
+            </a>
+            <a href='/'>
+              <li>Projects</li>
+            </a>
+            <a href='/'>
+              <li>About me</li>
+            </a>
+            <a href='/'>
+              <li>Contact</li>
+            </a>
           </ul>
-        </nav>
-        <main className='main'>
-        <section className='main_about'>
-          <h3>About</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate
-            nostrum reiciendis quidem labore exercitationem corporis
-            reprehenderit optio fugiat, eveniet ad voluptatibus quos totam quia
-            quo, modi, voluptatum eligendi facere architecto!
-          </p>
-        </section>
+        </div>
+      </nav>
+      <main className='main'>
         <section className='main_projects'>
           <div>
-            <h3>Personal projects</h3>
+            <h3>Proyectos personales</h3>
             <ul>
               <li>¡Oído cocina! Un juego de cartas.</li>
               <li>Japan travel planner</li>
@@ -37,7 +41,7 @@ function App() {
             </ul>
           </div>
           <div>
-            <h3>Projects with Adalab</h3>
+            <h3>Proyectos en Adalab</h3>
             <ul>
               <li>Módulo 1 - Evaluación final</li>
               <li>Módulo 1 - Trabajo grupal</li>
@@ -49,6 +53,33 @@ function App() {
               <li>Módulo 4 - Trabajo grupal</li>
             </ul>
           </div>
+        </section>
+        <section className='main_about'>
+          <h3>About</h3>
+
+          <p>
+            Motivated and eager to embark on a dynamic career in web
+            development, I am a Junior Front-end Developer who recently
+            completed the intensive AdaLab programming bootcamp, comprising 680
+            hours of immersive learning.
+          </p>
+
+          <p>
+            After six years of office work, I wholeheartedly decided to pursue
+            my passion for programming, an interest that has captivated me for
+            years. The Adalab course has been instrumental in honing my skills
+            in creating captivating and user-friendly web experiences. While my
+            focus has been primarily on front-end development, my curiosity has
+            led me to explore the intricacies of back-end programming, and I am
+            currently immersing myself in learning Python.
+          </p>
+
+          <p>
+            Driven by a passion for continuous learning and innovation, I am
+            excited to contribute my creativity to a dynamic team where I can
+            collaborate in developing cutting-edge solutions that elevate user
+            experiences to new heights.
+          </p>
         </section>
       </main>
       <footer className='footer'>©Raquel García | 2023</footer>
