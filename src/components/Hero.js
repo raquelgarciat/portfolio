c8dd776d282639934ee230325018e4661c93e5d6
raquelgarciat <raquelgarciat@gmail.com>
2023-09-05 13:39:39 +0200

    add project info

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,7 @@
 function Hero() {
   return (
     <section>
-      <article className='hero'>
+      <div className='hero'>
         <div>&lt; Hello world! /&gt;</div>
         <div>
           I'm <span>Raquel García</span>
@@ -9,7 +9,7 @@ function Hero() {
         <div className='container'>
           <div className='typewriter'>a junior front-end developer.</div>
         </div>
-      </article>
+      </div>
       <a href='#projects' className='scroll'>
         <i class='fa-solid fa-angles-down' aria-hidden='true'></i>
         <p>scroll down</p>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,6 @@
 function Projects() {
   return (
-    <section className='projects' id="projects">
+    <section className='projects' id='projects'>
       <section className='main_projects'>
         <div className='blurb'>
           <p>Thank you for stopping by!</p>
@@ -9,27 +9,37 @@ function Projects() {
         <ul>
           <li>
             <div className='project_img' id='blankimg'></div>
-            <p>¡Oído cocina! Un juego de cartas.</p>
+            <p>¡Oído cocina! A card game.</p>
             <p className='project_blurb'>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum
-              voluptates nemo vel dignissimos itaque suscipit atque.
+              Card game made in React.js, using a REST API for ingredient cards.
+              User has to keep ingredient cards in their hand to create full
+              dishes. Be careful of mice! :)
             </p>
+            <p>- work in progress -</p>
           </li>
           <li>
             <div className='project_img' id='blankimg'></div>
-            <p>Japan travel planner</p>
+            <p>Travel planner (Japan)</p>
             <p className='project_blurb'>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum
-              voluptates nemo vel dignissimos itaque suscipit atque.
+              Travel planner using React.js, personal project
+              for the organisation of an upcoming group trip.
+              User will be able to post tips, routes, places,
+              and any info they find useful for the trip.
+              Will feature a login for users, and a RESTful API
+              for data keeping.
             </p>
+            <p>- work in progress -</p>
           </li>
           <li>
             <div className='project_img' id='blankimg'></div>
-            <p>Warhammer 40k API (RESTful)</p>
+            <p>Warhammer 40k REST API</p>
             <p className='project_blurb'>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum
-              voluptates nemo vel dignissimos itaque suscipit atque.
+              RESTful API for data keeping of the newest edition
+              for the game Warhammer 40.000. Will feature data
+              for all characters, weapons, factions and more.
+              Data will be given in JSON format, and kept in MySQL.
             </p>
+            <p>- work in progress -</p>
           </li>
         </ul>
       </section>
@@ -61,51 +71,70 @@ function Projects() {
             </p>
           </li>
           <li>
-            <div className='project_img' id='blankimg'></div>
-            <p>Módulo 2 - Evaluación intermedia</p>
+            <div className='project_img' id='mod2-2'></div>
+            <p>Module 2 (JavaScript) - Mid-term evaluation</p>
             <p className='project_blurb'>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum
-              voluptates nemo vel dignissimos itaque suscipit atque.
+              Evaluation exercise for skill testing on JavaScript, consisting of
+              a betting dice game, user has to guess a number from 1 to 6,
+              betting a specific amount of given money.
             </p>
           </li>
           <li>
-            <div className='project_img' id='blankimg'></div>
-            <p>Módulo 2 - Evaluación final</p>
+            <div className='project_img' id='mod2-3'></div>
+            <p>Module 2 (JavaScript) - Final evaluation</p>
             <p className='project_blurb'>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum
-              voluptates nemo vel dignissimos itaque suscipit atque.
+              Disney character finder, retrieving data from an external API,
+              using 'fetch' on JavaScript, and including a section for favorite
+              characters. Keeps data on local storage.
             </p>
           </li>
           <li>
-            <div className='project_img' id='blankimg'></div>
-            <p>Módulo 2 - Trabajo grupal</p>
+            <div className='project_img' id='mod2-1'></div>
+            <p>Module 2 (JavaScript) - Group project</p>
             <p className='project_blurb'>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum
-              voluptates nemo vel dignissimos itaque suscipit atque.
+              Card creator with Animal Crossing visuals. User can create a card
+              using different color palettes and their personal information,
+              using a POST request on an external API.
             </p>
           </li>
           <li>
-            <div className='project_img' id='blankimg'></div>
-            <p>Módulo 3 - Evaluación final</p>
+            <div className='project_img' id='mod3-2'></div>
+            <p>Module 3 (React.js) - Mid-term evaluation</p>
             <p className='project_blurb'>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum
-              voluptates nemo vel dignissimos itaque suscipit atque.
+              Retrieving data from an external API, user can search for Friends
+              quotes, and filter by character. Extra option for posting a new
+              quote has been implemented, new quotes beting saved in local
+              storage.
             </p>
           </li>
           <li>
-            <div className='project_img' id='blankimg'></div>
-            <p>Módulo 3 - Trabajo grupal</p>
+            <div className='project_img' id='mod3-1'></div>
+            <p>Module 3 (React.js) - Final evaluation</p>
             <p className='project_blurb'>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum
-              voluptates nemo vel dignissimos itaque suscipit atque.
+              Retrieving data from an external API, user can search for
+              Rick&Morty characters by name and by species. Using React
+              components and routing, clicking on a character opens a detail
+              card with their info.
             </p>
           </li>
           <li>
-            <div className='project_img' id='blankimg'></div>
-            <p>Módulo 4 - Trabajo grupal</p>
+            <div className='project_img' id='mod3-3'></div>
+            <p>Module 3 (React.js) - Group project</p>
             <p className='project_blurb'>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum
-              voluptates nemo vel dignissimos itaque suscipit atque.
+              A page where user can post their project ideas, consisting of a
+              form that sends the data to an external API via POST method.
+              Changes are instantly implemented in a preview section.
+            </p>
+          </li>
+          <li>
+            <div className='project_img' id='mod4'></div>
+            <p>Module 4 (back end) - Group project</p>
+            <p className='project_blurb'>
+              Using the same idea as the last project, this SPA implements back
+              end with Express JS to create a server, in order for us to work
+              with our own REST API. The data collected in the form is POSTed to
+              a connected database in MySQL, feeding the API, and immediately
+              READ in the projects list page.
             </p>
           </li>
         </ul>
