bd361dde9eb6f55dda37a83d71b121a9569ff40b
raquelgarciat <raquelgarciat@gmail.com>
2023-09-05 14:56:00 +0200

    add project links icons and effects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -21,12 +21,11 @@ function Projects() {
             <div className='project_img' id='blankimg'></div>
             <p>Travel planner (Japan)</p>
             <p className='project_blurb'>
-              Travel planner using React.js, personal project
-              for the organisation of an upcoming group trip.
-              User will be able to post tips, routes, places,
-              and any info they find useful for the trip.
-              Will feature a login for users, and a RESTful API
-              for data keeping.
+              Travel planner using React.js, personal project for the
+              organisation of an upcoming group trip. User will be able to post
+              tips, routes, places, and any info they find useful for the trip.
+              Will feature a login for users, and a RESTful API for data
+              keeping.
             </p>
             <p>- work in progress -</p>
           </li>
@@ -34,10 +33,10 @@ function Projects() {
             <div className='project_img' id='blankimg'></div>
             <p>Warhammer 40k REST API</p>
             <p className='project_blurb'>
-              RESTful API for data keeping of the newest edition
-              for the game Warhammer 40.000. Will feature data
-              for all characters, weapons, factions and more.
-              Data will be given in JSON format, and kept in MySQL.
+              RESTful API for data keeping of the newest edition for the game
+              Warhammer 40.000. Will feature data for all characters, weapons,
+              factions and more. Data will be given in JSON format, and kept in
+              MySQL.
             </p>
             <p>- work in progress -</p>
           </li>
@@ -55,7 +54,15 @@ function Projects() {
         </div>
         <ul>
           <li>
-            <div className='project_img' id='mod1-1'></div>
+            <div class='container'>
+              <div className='project_img' id='mod1-1'></div>
+              <div class='middle'>
+                <div class='text'>
+                  <a href="https://www.w3schools.com/"><i class="fa-brands fa-github"></i></a>
+                  <a href="https://www.w3schools.com/"><i class="fa-solid fa-arrow-up-right-from-square"></i></a>
+                </div>
+              </div>
+            </div>
             <p>Module 1 (HTML / CSS) - Final evaluation</p>
             <p className='project_blurb'>
               Evaluation exercise for skill testing on web design, using HTML5
@@ -63,7 +70,15 @@ function Projects() {
             </p>
           </li>
           <li>
+          <div class='container'>
             <div className='project_img' id='mod1-2'></div>
+            <div class='middle'>
+                <div class='text'>
+                  <a href="https://www.w3schools.com/"><i class="fa-brands fa-github"></i></a>
+                  <a href="https://www.w3schools.com/"><i class="fa-solid fa-arrow-up-right-from-square"></i></a>
+                </div>
+              </div>
+            </div>
             <p>Module 1 (HTML / CSS) - Group project</p>
             <p className='project_blurb'>
               Group project using HTML5 and CSS3, following a Zeplin given
@@ -71,7 +86,15 @@ function Projects() {
             </p>
           </li>
           <li>
+          <div class='container'>
             <div className='project_img' id='mod2-2'></div>
+            <div class='middle'>
+                <div class='text'>
+                  <a href="https://www.w3schools.com/"><i class="fa-brands fa-github"></i></a>
+                  <a href="https://www.w3schools.com/"><i class="fa-solid fa-arrow-up-right-from-square"></i></a>
+                </div>
+              </div>
+            </div>
             <p>Module 2 (JavaScript) - Mid-term evaluation</p>
             <p className='project_blurb'>
               Evaluation exercise for skill testing on JavaScript, consisting of
@@ -80,7 +103,15 @@ function Projects() {
             </p>
           </li>
           <li>
+          <div class='container'>
             <div className='project_img' id='mod2-3'></div>
+            <div class='middle'>
+                <div class='text'>
+                  <a href="https://www.w3schools.com/"><i class="fa-brands fa-github"></i></a>
+                  <a href="https://www.w3schools.com/"><i class="fa-solid fa-arrow-up-right-from-square"></i></a>
+                </div>
+              </div>
+            </div>
             <p>Module 2 (JavaScript) - Final evaluation</p>
             <p className='project_blurb'>
               Disney character finder, retrieving data from an external API,
@@ -89,7 +120,15 @@ function Projects() {
             </p>
           </li>
           <li>
+          <div class='container'>
             <div className='project_img' id='mod2-1'></div>
+            <div class='middle'>
+                <div class='text'>
+                  <a href="https://www.w3schools.com/"><i class="fa-brands fa-github"></i></a>
+                  <a href="https://www.w3schools.com/"><i class="fa-solid fa-arrow-up-right-from-square"></i></a>
+                </div>
+              </div>
+            </div>
             <p>Module 2 (JavaScript) - Group project</p>
             <p className='project_blurb'>
               Card creator with Animal Crossing visuals. User can create a card
@@ -98,7 +137,15 @@ function Projects() {
             </p>
           </li>
           <li>
+          <div class='container'>
             <div className='project_img' id='mod3-2'></div>
+            <div class='middle'>
+                <div class='text'>
+                  <a href="https://www.w3schools.com/"><i class="fa-brands fa-github"></i></a>
+                  <a href="https://www.w3schools.com/"><i class="fa-solid fa-arrow-up-right-from-square"></i></a>
+                </div>
+              </div>
+            </div>
             <p>Module 3 (React.js) - Mid-term evaluation</p>
             <p className='project_blurb'>
               Retrieving data from an external API, user can search for Friends
@@ -108,7 +155,15 @@ function Projects() {
             </p>
           </li>
           <li>
+          <div class='container'>
             <div className='project_img' id='mod3-1'></div>
+            <div class='middle'>
+                <div class='text'>
+                  <a href="https://www.w3schools.com/"><i class="fa-brands fa-github"></i></a>
+                  <a href="https://www.w3schools.com/"><i class="fa-solid fa-arrow-up-right-from-square"></i></a>
+                </div>
+              </div>
+            </div>
             <p>Module 3 (React.js) - Final evaluation</p>
             <p className='project_blurb'>
               Retrieving data from an external API, user can search for
@@ -118,7 +173,15 @@ function Projects() {
             </p>
           </li>
           <li>
+          <div class='container'>
             <div className='project_img' id='mod3-3'></div>
+            <div class='middle'>
+                <div class='text'>
+                  <a href="https://www.w3schools.com/"><i class="fa-brands fa-github"></i></a>
+                  <a href="https://www.w3schools.com/"><i class="fa-solid fa-arrow-up-right-from-square"></i></a>
+                </div>
+              </div>
+            </div>
             <p>Module 3 (React.js) - Group project</p>
             <p className='project_blurb'>
               A page where user can post their project ideas, consisting of a
@@ -127,7 +190,15 @@ function Projects() {
             </p>
           </li>
           <li>
+          <div class='container'>
             <div className='project_img' id='mod4'></div>
+            <div class='middle'>
+                <div class='text'>
+                  <a href="https://www.w3schools.com/"><i class="fa-brands fa-github"></i></a>
+                  <a href="https://www.w3schools.com/"><i class="fa-solid fa-arrow-up-right-from-square"></i></a>
+                </div>
+              </div>
+            </div>
             <p>Module 4 (back end) - Group project</p>
             <p className='project_blurb'>
               Using the same idea as the last project, this SPA implements back
