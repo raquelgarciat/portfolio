c0254c61dd12b82d9ea58b592ba4589d507d8ab4
raquelgarciat <raquelgarciat@gmail.com>
2023-09-07 14:34:35 +0200

    add contact section

--- src/components/About.js
+++ src/components/About.js
@@ -13,20 +13,20 @@ function About() {
               I'm based in Madrid, Spain <i class='fa-solid fa-globe'></i>
             </li>
             <li>
-              I speak Spanish, Catalan and English{' '}
+              I speak Spanish, Catalan and English fluently{' '}
               <i class='fa-solid fa-language'></i>
             </li>
             <li>
               I love working with JavaScript, React.js and MySQL{' '}
-              <i class="fa-brands fa-react"></i>
+              <i class='fa-brands fa-react'></i>
             </li>
             <li>
               I'm currently interested in Python as well{' '}
-              <i class="fa-brands fa-python"></i>
+              <i class='fa-brands fa-python'></i>
             </li>
             <li>
-              I never want to stop learning! {' '}
-              <i class="fa-solid fa-graduation-cap"></i>
+              I never want to stop learning!{' '}
+              <i class='fa-solid fa-graduation-cap'></i>
             </li>
             <li>
               I prefer to work remotely <i class='fa-solid fa-house-laptop'></i>
@@ -36,11 +36,40 @@ function About() {
               <i class='fa-regular fa-thumbs-up'></i>
             </li>
           </ul>
+          <div className='about_links'>
+            <a target="blank" 
+              href='http://www.linkedin.com/in/raquel-g-881b20285'
+            >
+              <i class='fa-brands fa-linkedin'></i>
+            </a>
+            <a target="blank" 
+              href='http://github.com/raquelgarciat'
+            >
+            <i class='fa-brands fa-square-github'></i></a>
+            <a target="blank" title="Download my CV!"
+              href='https://pdfhost.io/v/Ld~~o.Q1h_Raquel_Garcia_Torrejon_Resume'
+            >
+            <i class='fa-solid fa-file-arrow-down'></i></a>
+          </div>
         </div>
       </article>
       <div className='stack'>
         <ul>
-          <li>logo</li>
+          <li id="stack_html" title="HTML5"></li>
+          <li id="stack_css" title="CSS3"></li>
+          <li id="stack_sass" title="SASS"></li>
+          <li id="stack_js" title="JavaScript"></li>
+          <li id="stack_react" title="React.js"></li>
+          <li id="stack_jest" title="Jest"></li>
+          <li id="stack_express" title="Espress JS"></li>
+          <li id="stack_node" title="Node JS"></li>
+          <li id="stack_mysql" title="MySQL"></li>
+          <li id="stack_mongodb" title="MongoDB"></li>
+          <li id="stack_json" title=".JSON format"></li>
+          <li id="stack_vscode" title="Visual Studio Code"></li>
+          <li id="stack_git" title="Git"></li>
+          <li id="stack_github" title="GitHub"></li>
+          <li id="stack_scrum" title="Scrum"></li>
         </ul>
       </div>
     </section>
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,61 @@
 function Contact() {
-    return (
-      <section className='contact' id="contact">
-      </section>
-    );
-  }
-  
-  export default Contact;
+  return (
+    <section className='contact' id='contact'>
+      <div className='contact_container'>
+        <h3>Get in touch!</h3>
+        <p>I'd love to hear from you :)</p>
+        <form>
+          <div className='form_container'>
+            <fieldset>
+              <label htmlFor='firstName'>First name</label>
+              <input
+                placeholder='Name *'
+                id='firstName'
+                type='text'
+                name='firstName'
+                required
+              />
+            </fieldset>
+            <fieldset>
+              <label htmlFor='email'>Your email</label>
+              <input
+                placeholder='Email *'
+                id='email'
+                type='email'
+                name='email'
+                required
+              />
+            </fieldset>
+          </div>
+          <fieldset>
+            <label htmlFor='subject'>Subject</label>
+            <input
+              placeholder='Subject *'
+              id='email'
+              type='email'
+              name='email'
+              required
+            />
+          </fieldset>
+          <fieldset>
+            <label for='message'>Your message</label>
+            <textarea
+              id='message'
+              name='message'
+              rows='8'
+              cols='80'
+              placeholder='Your message *'
+              required
+            ></textarea>
+          </fieldset>
+          <div className="button_container">
+            <input className='button' type='reset' value='Clear all' />
+            <input className='button' type='submit' value='Send message' />
+          </div>
+        </form>
+      </div>
+    </section>
+  );
+}
+
+export default Contact;
